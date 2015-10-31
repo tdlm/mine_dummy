c0cda80325156ed72a02604b0db001a62d57963b
Scott Weaver <scottmweaver@gmail.com>
2015-10-31 02:09:37 -0700

    JSDoc for Event Manager.

--- js/event_manager.js
+++ js/event_manager.js
@@ -1,9 +1,20 @@
 'use strict';
 
+/**
+ * Event Manager
+ *
+ * @constructor
+ */
 function EventManager() {
     this.listeners = [];
 }
 
+/**
+ * Trigger Event Callbacks
+ *
+ * @param event
+ * @param data
+ */
 EventManager.prototype.trigger = function(event, data) {
     for (var i = 0; i < this.listeners.length; i++) {
         var listener = this.listeners[i];
@@ -14,6 +25,12 @@ EventManager.prototype.trigger = function(event, data) {
     }
 };
 
+/**
+ * Add Listener Callbacks for an Event
+ *
+ * @param event
+ * @param callback
+ */
 EventManager.prototype.addListener = function(event, callback) {
     this.listeners.push({event: event, callback: callback});
 };
