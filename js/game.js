826412f69f2cb1277d5f7c4b44e02a2ba3bbf654
Scott Weaver <scottmweaver@gmail.com>
2015-11-02 17:37:28 -0800

    Default Minesweeper setting (30x16, 99 mines)

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 window.requestAnimationFrame(function() {
-    window.GM = new GameManager(9, 9, 10,
+    window.GM = new GameManager(30, 16, 99,
         {
             'table_id': 'game_grid',
             'start_button_id': 'game_start',
